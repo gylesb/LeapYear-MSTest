6e26c441207b8db666fd9147500020c13b8d9415
Gyles Batara <gyles.batara@live.com>
2017-10-17 09:19:15 -0700

    Add ModelTests directory and LeapYearTest cs file

---
+++ LeapYear.Tests/ModelTests/LeapYearTest.cs
@@ -0,0 +1,15 @@
+using Microsoft.Visual.Studio.TestTools.UnitTesting;
+using LeapYear;
+
+namespace LeapYear.TestTools
+{
+  [TestClass]
+  public class LeapYearTest
+  {
+    [TestMethod]
+    public void IsLeapYear_NumberDivisibleByFour_True()
+    {
+      // code
+    }
+  }
+}
